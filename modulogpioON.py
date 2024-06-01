5356b15c1da48ca7f704dc5de5c7a822628b16c3
gioboto <33821206+gioboto@users.noreply.github.com>
2024-05-31 23:10:01 -0500

    Create modulogpioON.py

---
+++ modulogpioON.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+def relayon():
+
+    pinList = [3]
+
+    for i in pinList:
+        GPIO.setup(i, GPIO.OUT)
+        #i =- 1
+
+    try:
+        GPIO.output(i, GPIO.LOW)
+        print ("turned on")
+
+    except KeyboardInterrupt:
+        print ("Quit")
+        GPIO.cleanup()
+
+#reon = relayon()
